2be04b32a4345f6f2959645ff385201f14223874
Vitor Cesar <vitor@fasters.com.br>
2023-04-26 15:11:38 -0300

    feat: implement prisma to app modulke

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './modules/user/user.controller';
+import { UserService } from './modules/user/user.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
