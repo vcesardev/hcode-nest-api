be41ef95828df6e0adfc41b7b9a0ad59cc19a2a3
Vitor Cesar <vitor@fasters.com.br>
2023-07-10 14:24:15 -0300

    feat: add models

--- src/types/User.ts
+++ src/types/User.ts
@@ -2,3 +2,12 @@ export enum Role {
   User = 1,
   Admin = 2,
 }
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  role: Role;
+  createdAt: string;
+  updatedAt: string;
+};
