b6fcc8c3e3ee412f780a0a30eeda57f5e565abf7
Vitor Cesar <vitor@fasters.com.br>
2023-07-18 12:31:16 -0300

    feat: add mail service module to app.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
 import { FilesModule } from './modules/files/files.module';
 
 @Module({
@@ -15,6 +20,26 @@ import { FilesModule } from './modules/files/files.module';
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule),
     forwardRef(() => FilesModule),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'halie22@ethereal.email',
+          pass: 'DgAG628qh8vbaDjFjA',
+        },
+      },
+      defaults: {
+        from: '"Vitor emails" <halie22@ethereal.email>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [
